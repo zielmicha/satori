31f00efb8204784e61fb40921005a42d3a4a119a
duraj <duraj@tcs.uj.edu.pl>
2010-06-19 13:29:36 +0200

    Models stubization, fixed urls.py hangup.

---
+++ satori.core/satori/core/models/_Contestant.py
@@ -0,0 +1,23 @@
+from django.db import models
+from satori.dbev import events
+from satori.ars import django_
+from satori.core.models._Role import Role
+
+class Contestant(Role):
+    """Model. A Role for a contest participant.
+    """
+    __module__ = "satori.core.models"
+
+    contest    = models.ForeignKey('Contest')
+    user       = models.ForeignKey('User')
+    accepted   = models.BooleanField()
+
+
+class ContestantEvents(events.Events):
+    model = Contestant
+    on_insert = on_update = ['name', 'contest']
+    on_delete = []
+
+class ContestantOpers(django_.Opers):
+    contestant = django_.ModelProceduresProvider(Contestant)
+
