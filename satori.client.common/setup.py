6ffe0862f3e337a7cefae09bdf6314b13831dedd
duraj <duraj@tcs.uj.edu.pl>
2010-07-22 12:27:27 +0200

    Merge failed, trying again.

---
+++ satori.client.common/setup.py
@@ -0,0 +1,20 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+from setuptools import setup, find_packages
+
+setup(name='satori.client.common',
+    packages=find_packages(),
+    namespace_packages=[
+        'satori',
+        'satori.client',
+    ],
+    install_requires=[
+        'setuptools',
+        'Thrift',
+        'satori.ars',
+        'satori.objects',
+    ],
+    entry_points='''
+        [console_scripts]
+        satori.client.console = satori.client.common:start_console
+    ''',
+)
