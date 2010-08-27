c297bc3d13f9f56379650f8c5bf236e031e4f9dc
marek j wrobel <marek.j.wrobel@gmail.com>
2010-08-27 14:53:55 +0200

    Fixed NullableArsStruct, cleaned ThriftClient, refactored satori.client.common, changed create() and filter() functions to use structures in arguments.

---
+++ satori.core/satori/core/models/_TestSuite.py
@@ -0,0 +1,40 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+
+from django.db import models
+from satori.dbev import Events
+from satori.core.models._Object import Object
+from satori.core.models.modules import DISPATCHERS
+from datetime import datetime
+
+class TestSuite(Object):
+    """Model. A group of tests, with dispatch and aggregation algorithm.
+    """
+    __module__ = "satori.core.models"
+    parent_object = models.OneToOneField(Object, parent_link=True, related_name='cast_testsuite')
+
+    problem     = models.ForeignKey('Problem')
+    name        = models.CharField(max_length=50)
+    description = models.TextField(blank=True, default="")
+    tests       = models.ManyToManyField('Test', through='TestMapping')
+    dispatcher  = models.CharField(max_length=128, choices=DISPATCHERS)
+    
+    def inherit_right(self, right):
+        right = str(right)
+        ret = super(TestSuite, self).inherit_right(right)
+        if right=='EDIT':
+            ret.append((self.problem,'EDIT'))
+        return ret
+
+    def save(self, *args, **kwargs):
+        if not self.name:
+            self.name = str(datetime.now())
+        super(TestSuite,self).save(*args,**kwargs)
+        
+    class Meta:                                                # pylint: disable-msg=C0111
+        unique_together = (('problem', 'name'),)
+
+class TestSuiteEvents(Events):
+    model = TestSuite
+    on_insert = on_update = ['owner', 'problem', 'name']
+    on_delete = []
+
