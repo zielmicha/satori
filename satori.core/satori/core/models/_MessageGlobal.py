420e7f318bdbd57c54588bd065ad041340c38dc0
kalq28 <kalq28@gmail.com>
2010-08-28 23:56:48 +0200

    Reverted script that makes auto imports in widgets/__init__.py.

---
+++ satori.core/satori/core/models/_MessageGlobal.py
@@ -0,0 +1,22 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+
+from django.db import models
+from satori.dbev import Events
+from satori.core.models._Message import Message
+
+class MessageGlobal(Message):
+    """Model. Description of a text message - main screen msg.
+    """
+    __module__ = "satori.core.models"
+    parent_message = models.OneToOneField(Message, parent_link=True, related_name='cast_messageglobal')
+
+    mainscreenonly = models.BooleanField()
+    
+    def __str__(self):
+        return self.topic+" (Global)"
+
+class MessageGlobalEvents(Events):
+    model = MessageGlobal
+    on_insert = on_update = ['topic', 'time']
+    on_delete = []
+
