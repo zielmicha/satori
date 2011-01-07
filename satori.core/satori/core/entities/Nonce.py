6a460a87ae3ba7ea4d312ed7f775e55953fb769c
marek j wrobel <marek.j.wrobel@gmail.com>
2011-01-07 16:48:12 +0100

    Entity.update_fields() returns changed fields.

---
+++ satori.core/satori/core/entities/Nonce.py
@@ -0,0 +1,13 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+
+from django.db import models
+
+class Nonce(models.Model):
+    """
+    """
+    server_url = models.CharField(max_length=2048)
+    timestamp  = models.IntegerField()
+    salt       = models.CharField(max_length=64)
+
+    class Meta:
+        unique_together = (('server_url', 'timestamp', 'salt'),)
