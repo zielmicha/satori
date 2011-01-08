a0aa413c17ef48c5a8544175d2a80f6274c11796
marek j wrobel <marek.j.wrobel@gmail.com>
2011-01-08 10:13:38 +0100

    Small fixes to OaMap.

---
+++ satori.client.common/satori/client/common/remote.py
@@ -0,0 +1,159 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+
+import getpass
+import os
+import shutil
+import sys
+import urllib
+from httplib import HTTPConnection
+from StringIO import StringIO
+from types import FunctionType
+
+from thrift.transport.TSocket import TSocket
+
+from satori.ars.model import ArsString, ArsProcedure, ArsService, ArsInterface
+from satori.ars.thrift import ThriftClient, ThriftReader
+from satori.objects import Argument, Signature, ArgumentMode
+from unwrap import unwrap_interface
+from oa_map import get_oa_map
+from token_container import token_container
+
+if getpass.getuser() == 'gutowski':
+    client_host = 'satori.tcs.uj.edu.pl'
+    client_port = 38889
+    blob_port = 38887
+elif (getpass.getuser() == 'zzzmwm01') or (getpass.getuser() == 'mwrobel'):
+    client_host = 'localhost'
+    client_port = 37889
+    blob_port = 37887
+elif getpass.getuser() == 'duraj':
+    client_host = 'localhost'
+    client_port = 36889
+    blob_port = 36887
+else:
+    client_host = 'satori.tcs.uj.edu.pl'
+    client_port = 38889
+    blob_port = 38887
+
+def transport_factory():
+    return TSocket(host=client_host, port=client_port)
+
+@Argument('transport_factory', type=FunctionType)
+def bootstrap_thrift_client(transport_factory):
+    interface = ArsInterface()
+    idl_proc = ArsProcedure(return_type=ArsString, name='Server_getIDL')
+    idl_serv = ArsService(name='Server')
+    idl_serv.add_procedure(idl_proc)
+    interface.add_service(idl_serv)
+
+    bootstrap_client = ThriftClient(interface, transport_factory)
+    bootstrap_client.wrap_all()
+    idl = idl_proc.implementation()
+    bootstrap_client.stop()
+
+    idl_reader = ThriftReader()
+    interface = idl_reader.read_from_string(idl)
+
+    client = ThriftClient(interface, transport_factory)
+    client.wrap_all()
+
+    return (interface, client)
+
+class BlobWriter(object):
+    def __init__(self, length, model=None, id=None, name=None, group=None, filename=''):
+        if model:
+            url = '/blob/{0}/{1}/{2}/{3}'.format(urllib.quote(model), str(id), urllib.quote(group), urllib.quote(name))
+        else:
+            url = '/blob/upload'
+
+        headers = {}
+        headers['Host'] = urllib.quote(client_host)
+        headers['Cookie'] = 'satori_token=' + urllib.quote(token_container.get_token())
+        headers['Content-length'] = str(length)
+        headers['Filename'] = urllib.quote(filename)
+
+        self.con = HTTPConnection(client_host, blob_port)
+        try:
+            self.con.request('PUT', url, '', headers)
+        except:
+            self.con.close()
+            raise
+
+    def write(self, data):
+        try:
+            ret = self.con.send(data)
+        except:
+            self.con.close()
+            raise
+        return ret
+
+    def close(self):
+        try:
+            res = self.con.getresponse()
+            if res.status != 200:
+                raise Exception("Server returned %d (%s) answer." % (res.status, res.reason))
+            length = int(res.getheader('Content-length'))
+            ret = res.read(length)
+        finally:
+            self.con.close()
+        return ret
+
+class BlobReader(object):
+    def __init__(self, model=None, id=None, name=None, group=None, hash=None):
+        if model:
+            url = '/blob/{0}/{1}/{2}/{3}'.format(urllib.quote(model), str(id), urllib.quote(group), urllib.quote(name))
+        else:
+            url = '/blob/download/{0}'.format(urllib.quote(hash))
+
+        headers = {}
+        headers['Host'] = urllib.quote(client_host)
+        headers['Cookie'] = 'satori_token=' + urllib.quote(token_container.get_token())
+        headers['Content-length'] = '0'
+
+        try:
+            self.con = HTTPConnection(client_host, blob_port)
+            self.con.request('GET', url, '', headers)
+
+            self.res = self.con.getresponse()
+            if self.res.status != 200:
+                raise Exception("Server returned %d (%s) answer." % (self.res.status, self.res.reason))
+            self.length = int(self.res.getheader('Content-length'))
+            self.filename = urllib.unquote(self.res.getheader('Filename', ''))
+        except:
+            self.con.close()
+            raise
+
+    def read(self, len):
+        try:
+            ret = self.res.read(len)
+        except:
+            self.con.close()
+            raise
+        return ret
+
+    def close(self):
+        self.con.close()
+
+def anonymous_blob(length):
+    return BlobWriter(length)
+def anonymous_blob_path(path):
+    with open(path, 'r') as src:
+        ln = os.fstat(src.fileno()).st_size
+        blob = anonymous_blob(ln)
+        shutil.copyfileobj(src, blob, ln)
+    return blob.close()
+
+print 'Bootstrapping client...'
+
+(_interface, _client) = bootstrap_thrift_client(transport_factory)
+_classes = unwrap_interface(_interface, BlobReader, BlobWriter)
+
+_module = sys.modules[__name__]
+for name, value in _classes.iteritems():
+    setattr(_module, name, value)
+
+OaMap = get_oa_map(_classes['Attribute'], _classes['AnonymousAttribute'], _classes['BadAttributeType'], _classes['Blob'])
+
+setattr(_module, '__all__', _classes.keys() + ['token_container', 'anonymous_blob', 'anonymous_blob_path', 'OaMap'])
+
+print 'Client bootstrapped.'
---
+++ satori.client.web/satori/client/web/requests/_RegisterRequest.py
@@ -0,0 +1,46 @@
+﻿# vim:ts=4:sts=4:sw=4:expandtab
+from satori.client.web.URLDictionary import *
+from satori.client.web.queries import *
+from django.db import models
+from django.conf import settings
+from satori.client.web.librecaptcha import *
+from satori.client.common.remote import *
+from _Request import Request
+
+class RegisterRequest(Request):
+    pathName = 'register'
+    @classmethod
+    def process(cls, request):
+        vars = request.REQUEST
+        d = ParseURL(vars.get('back_to', ''))
+        path = vars.get('path', '')
+        login = vars['username']
+        password = vars['password']
+        confirm = vars['confirm']
+        fullname = vars['fullname']
+        email = vars['email']
+        lw_path = vars['lw_path']
+        remote_ip = request.META['REMOTE_ADDR']
+        challenge = request.POST['recaptcha_challenge_field']
+        response = request.POST['recaptcha_response_field']
+        recaptcha_response = submit(challenge, response, settings.RECAPTCHA_PRIV_KEY, remote_ip)        
+        error = False
+        if password!=confirm:
+            error = 'nomatch'
+        elif not recaptcha_response.is_valid:
+            error = 'captchafail'
+        else:
+            try:
+                User.register(UserStruct(login=login, email=email, name=fullname), password=password)
+            except InvalidLogin:
+                error = 'badlogin'
+            except InvalidPassword:
+                error = 'badpass'
+            except InvalidEmail:
+                error = 'bademail'
+            except:
+                error = 'regfail'
+        if error:
+            return GetLink(DefaultLayout(maincontent='registerform',status=[error]),'')
+        else:
+            return GetLink(DefaultLayout(maincontent='loginform',status=['waiting']),'')
