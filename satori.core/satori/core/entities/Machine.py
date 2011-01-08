7fa859c2a09abba85a6a009e9add940e930b97a0
gzegzol <gzegzol@gmail.com>
2011-01-08 10:41:58 +0100

    ProblemMapping has new create

---
+++ satori.core/satori/core/entities/Machine.py
@@ -0,0 +1,78 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+
+import crypt
+import random
+import string
+import ipaddr
+
+from satori.ars.server  import server_info
+from satori.core.dbev   import Events
+from satori.core.models import Role, LoginFailed, InvalidLogin, login_ok, password_ok, password_crypt, password_check
+from satori.core.models import LoginFailed
+
+@ExportModel
+class Machine(Role):
+    """Model. A Machine.
+    """
+    parent_role = models.OneToOneField(Role, parent_link=True, related_name='cast_machine')
+
+    login       = models.CharField(max_length=64, unique=True)
+    password    = models.CharField(max_length=128, null=True)
+    address     = models.IPAddressField()
+    netmask     = models.IPAddressField(default='255.255.255.255')
+
+    class ExportMeta(object):
+        fields = [('login', 'VIEW'), ('address', 'VIEW'), ('netmask', 'VIEW')]
+
+    def save(self, *args, **kwargs):
+        login_ok(self.login)
+        if Machine.objects.filter(login=self.login).exclude(id=self.id):
+            raise InvalidLogin(login=self.login, reason='is already used')
+        super(Machine, self).save(*args, **kwargs)
+    
+    @ExportMethod(DjangoStruct('Machine'), [DjangoStruct('Machine')], PCGlobal('MANAGE'), [InvalidLogin, CannotSetField])
+    @staticmethod
+    def create(fields):
+        machine = Machine()
+        machine.forbid_fields(fields, ['id'])
+        machine.update_fields(fields, ['name', 'login', 'address', 'netmask'])
+        machine.save()
+        return machine
+
+    @ExportMethod(DjangoStruct('Machine'), [DjangoId('Machine'), DjangoStruct('Machine')], PCArg('self', 'MANAGE'), [CannotSetField, InvalidLogin])
+    def modify(self, fields):
+        self.forbid_fields(fields, ['id'])
+        self.update_fields(fields, ['name', 'login', 'address', 'netmask'])
+        self.save()
+        return self
+
+    @ExportMethod(unicode, [unicode, unicode], PCPermit())
+    @staticmethod
+    def authenticate(login, password):
+        try:
+            machine = Machine.objects.get(login=login)
+        except Machine.DoesNotExist:
+            raise LoginFailed()
+        if not machine.check_ip(server_info.client_ip):
+            raise LoginFailed()
+        if not check_password(machine.password, password):
+            raise LoginFailed()
+        session = Session.start()
+        session.login(machine, 'machine')
+        return str(token_container.token)
+
+    @ExportMethod(NoneType, [DjangoId('Machine'), unicode], PCArg('self', 'MANAGE'))
+    def set_password(self, new_password):
+        password_ok(new_password)
+        self.password = password_crypt(new_password)
+        self.save()
+
+    @ExportMethod(NoneType, [DjangoId('Machine'), unicode], PCArg('self', 'MANAGE'))
+    def check_ip(self, ip):
+        net = ipaddr.IPv4Network(self.address + '/' + self.netmask)
+        addr = ipaddr.IPv4Address(ip)
+        return addr in net
+
+class MachineEvents(Events):
+    model = Machine
+    on_insert = on_update = on_delete = []
---
+++ satori.core/satori/core/entities/TestMapping.py
@@ -0,0 +1,32 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+
+from django.db import models
+
+from satori.core.dbev   import Events
+from satori.core.models import Entity
+
+@ExportModel
+class TestMapping(Entity):
+    """Model. Intermediary for many-to-many relationship between TestSuites and
+    Tests.
+    """
+    parent_entity = models.OneToOneField(Entity, parent_link=True, related_name='cast_testmapping')
+
+    suite         = models.ForeignKey('TestSuite', related_name='test_mappings+')
+    test          = models.ForeignKey('Test', related_name='test_mappings+')
+    order         = models.IntegerField()
+
+    class Meta:                                                # pylint: disable-msg=C0111
+        unique_together = (('suite', 'test'), ('suite', 'order'))
+        ordering        = ('order',)
+
+    class ExportMeta(object):
+        fields = [('suite', 'VIEW'), ('test', 'VIEW'), ('order', 'VIEW')]
+
+    def __str__(self):
+        return str(self.order)+": ("+self.suite.name+","+self.test.name+")"
+
+class TestMappingEvents(Events):
+    model = TestMapping
+    on_insert = on_update = ['suite', 'test']
+    on_delete = []
