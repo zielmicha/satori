5cd8dd00c081950e8b6f0df782d799d934d63785
gzegzol <gzegzol@gmail.com>
2011-01-08 22:38:48 +0100

    ugly bug fixed.

---
+++ satori.client.web/satori/client/web/requests/_AcceptUserRequest.py
@@ -0,0 +1,15 @@
+﻿# vim:ts=4:sts=4:sw=4:expandtab
+from satori.client.web.URLDictionary import *
+from satori.client.web.queries import *
+from django.db import models
+from satori.client.common.remote import *
+from _Request import Request
+
+class AcceptUserRequest(Request):
+    pathName = 'accept'
+    @classmethod
+    def process(cls, request):
+        cu = Contestant.filter({'id':int(request.POST['conuser_id'])})[0]
+        cu.accepted = True
+        d = ParseURL(request.POST['back_to'])
+        return GetLink(d,'')
---
+++ satori.client.web/satori/client/web/widgets/_NewsWidget.py
@@ -0,0 +1,27 @@
+﻿from satori.client.web.queries import *
+from satori.client.common.remote import *
+from _Widget import Widget
+
+from docutils.core import publish_parts
+
+# news table (a possible main content)
+class NewsWidget(Widget):
+    pathName = 'news'
+    def __init__(self, params, path):
+        self.htmlFile = 'htmls/news.html'
+        c = ActiveContest(params)
+        if c:
+            messages = Subpage.get_for_contest(c, True)
+        else:
+            messages = Subpage.get_global(True)
+        self.allmsg = []
+        for m in messages:
+            self.allmsg.append({'m' : m, 'content' : text2html(m.content)})
+#        for m in MessageGlobal.filter():
+#            if not ActiveContest(params) or not m.mainscreenonly:
+#                
+#        if (ActiveContest(params)):
+#            for m in MessageContest.filter({'contest':ActiveContest(params)}):
+#                self.messages.append({'time' : m.time, 'type' : 'contest', 'topic' : m.topic, 'content' : publish_parts(m.content, writer_name='html')['fragment']})
+        self.allmsg.sort(key=lambda msg: msg['m'].date_created, reverse=True)
+
