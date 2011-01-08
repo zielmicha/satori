a4ad3994910bffc5747a6fe7fa338234028a93c6
marek j wrobel <marek.j.wrobel@gmail.com>
2011-01-08 23:32:24 +0100

    Rejudge fix.

---
+++ satori.client.web/satori/client/web/requests/_CreatePMRequest.py
@@ -0,0 +1,21 @@
+﻿# vim:ts=4:sts=4:sw=4:expandtab
+from satori.client.web.URLDictionary import *
+from satori.client.web.queries import *
+from django.db import models
+from satori.client.common.remote import *
+from _Request import Request
+from datetime import datetime
+
+
+class CreatePMRequest(Request):
+    pathName = 'createpm'
+    @classmethod
+    def process(cls,request):
+        contest = ContestById(int(request.POST['contest']))
+        problem = Problem.filter({'id' : int(request.POST['problem'])})[0]
+        tss = TestSuiteStruct(name='All tests '+str(datetime.now()),problem=problem,dispatcher='SerialDispatcher',accumulators='StatusAccumulator')
+        fullts = TestSuite.create(tss,Test.filter(TestStruct(problem=problem)))
+        ProblemMapping.create(ProblemMappingStruct(contest=contest, problem=problem,code=request.POST['code'], title=request.POST['title'],default_test_suite=fullts))
+        d = ParseURL(request.POST['back_to'])
+        return GetLink(d,'')
+    
---
+++ satori.client.web/satori/client/web/requests/_ExternalIdentityAddRequest.py
@@ -0,0 +1,22 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+from satori.client.web.URLDictionary import *
+from satori.client.web.queries import *
+from django.db import models
+from satori.client.common.remote import *
+from _Request import Request
+
+class ExternalIdentityAddRequest(Request):
+    pathName = 'exid_add'
+    @classmethod
+    def process(cls, request):
+        vars = request.REQUEST
+        back_to = vars.get('back_to', '')
+        path = vars.get('path', '')
+        lw_path = vars.get('lw_path', '')
+        salt = vars.get('salt', '')
+        d = ParseURL(back_to)
+        try:
+            ExternalIdentity.add(salt=salt)
+        except:
+            follow(d,lw_path)['status'] = ['failed']
+        return GetLink(d,path)
