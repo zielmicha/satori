1df39af39f287e9d5c9310fb58e9b227b1faca2b
gzegzol <gzegzol@gmail.com>
2011-01-15 02:59:01 +0100

    missing import bug-fix.

---
+++ satori.client.web/satori/client/web/widgets/_RankingWidget.py
@@ -0,0 +1,15 @@
+﻿from satori.client.web.queries import *
+from satori.client.common.remote import *
+from _Widget import Widget
+
+from docutils.core import publish_parts
+
+# ranking (a possible main content)
+class RankingWidget(Widget):
+    pathName = 'ranking'
+    def __init__(self, params, path):
+        self.htmlFile = 'htmls/ranking.html'
+        d = follow(params,path)
+        r = Ranking.filter({'id' : int(d['id'][0])})[0]
+        self.name = r.name
+        self.content = text2html(r.full_ranking())
