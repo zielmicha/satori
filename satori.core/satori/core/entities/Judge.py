0853663b15ce21f36c9c2ede227c0213373a7500
gzegzol <gzegzol@gmail.com>
2011-02-11 15:06:25 +0100

    missing save.

---
+++ satori.core/satori/core/entities/Judge.py
@@ -0,0 +1,80 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+
+"""
+Judge helper procedures.
+"""
+
+from types import NoneType
+
+from satori.core.dbev   import Events
+from satori.core.models import Role
+from satori.events      import Event
+
+SubmitToCheck = Struct('SubmitToCheck', (
+    ('test_result', long, True),
+    ('test_data', TypedMap(unicode, AnonymousAttribute), False),
+    ('submit_data', TypedMap(unicode, AnonymousAttribute), False),
+))
+
+@ExportClass
+class Judge(object):
+    """
+    """
+    @ExportMethod(SubmitToCheck, [], PCGlobal('JUDGE'))
+    @staticmethod
+    def get_next():
+        from satori.core.checking.check_queue_client import check_queue_client
+        r = token_container.token.role
+
+        next = check_queue_client.get_next(r)
+
+        if next.test_result_id is None:
+            return None
+
+        ret = SubmitToCheck()
+        ret.test_result = next.test_result_id
+
+        if next.test_result_id > 0:
+            test_result = TestResult.objects.get(id=next.test_result_id)
+            ret.test_data = test_result.test.data_get_map()
+            ret.submit_data = test_result.submit.data_get_map()
+        else:
+            temporary_submit = TemporarySubmit.objects.get(id=-next.test_result_id)
+            ret.test_data = temporary_submit.test_data_get_map()
+            ret.submit_data = temporary_submit.submit_data_get_map()
+
+        return ret
+
+    @ExportMethod(NoneType, [long, TypedMap(unicode, AnonymousAttribute)], PCGlobal('JUDGE'))
+    @staticmethod
+    def set_partial_result(test_result_id, result):
+        if test_result_id > 0:
+            test_result = TestResult.objects.get(id=test_result_id)
+            if test_result.tester != token_container.token.role:
+                return
+            test_result.oa_set_map(result)
+        else:
+            temporary_submit = TemporarySubmit.objects.get(id=-test_result_id)
+            if temporary_submit.tester != token_container.token.role:
+                return
+            temporary_submit.result_set_map(result)
+
+    @ExportMethod(NoneType, [long, TypedMap(unicode, AnonymousAttribute)], PCGlobal('JUDGE'))
+    @staticmethod
+    def set_result(test_result_id, result):
+        if test_result_id > 0:
+            test_result = TestResult.objects.get(id=test_result_id)
+            if test_result.tester != token_container.token.role:
+                return
+            test_result.oa_set_map(result)
+            test_result.pending = False
+            test_result.save()
+            RawEvent().send(Event(type='checking_checked_test_result', id=test_result.id))
+        else:
+            temporary_submit = TemporarySubmit.objects.get(id=-test_result_id)
+            if temporary_submit.tester != token_container.token.role:
+                return
+            temporary_submit.result_set_map(result)
+            temporary_submit.pending = False
+            temporary_submit.save()
+        
---
+++ satori.core/satori/core/entities/Subpage.py
@@ -0,0 +1,89 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+
+from django.db import models
+
+from satori.core.dbev   import Events
+from satori.core.models import Entity
+
+@ExportModel
+class Subpage(Entity):
+    """Model. Subpage or announcement. Can be tied to a contest.
+    """
+    parent_entity = models.OneToOneField(Entity, parent_link=True, related_name='cast_subpage')
+    
+    contest       = models.ForeignKey('Contest', related_name='subpages', null=True)
+    is_public     = models.BooleanField(default=True)
+    is_everywhere = models.BooleanField(default=False)
+    is_announcement = models.BooleanField(default=False)
+    name          = models.TextField(blank=False)
+    content       = models.TextField()
+    order         = models.IntegerField(default=0)
+    date_created  = models.DateTimeField(auto_now_add=True)
+
+    class Meta:                                                # pylint: disable-msg=C0111
+        unique_together = (('contest', 'name'),)
+        ordering        = ('order',)
+
+    class ExportMeta(object):
+        fields = [('contest', 'VIEW'), ('is_public', 'VIEW'), ('is_everywhere', 'VIEW'), ('is_announcement', 'VIEW'), ('name', 'VIEW'), ('content', 'VIEW'), ('order', 'VIEW'), ('date_created', 'VIEW')]
+
+    @classmethod
+    def inherit_rights(cls):
+        inherits = super(Subpage, cls).inherit_rights()
+        cls._inherit_add(inherits, 'VIEW', 'contest', 'VIEW', 'is_public', '1')
+        cls._inherit_add(inherits, 'MANAGE', 'contest', 'MANAGE')
+        # TODO: conditional inherit: if contest is set and is_public, inherit VIEW from contest
+        return inherits
+
+    @ExportMethod(DjangoStruct('Subpage'), [DjangoStruct('Subpage')], PCGlobal('ADMIN'), [CannotSetField])
+    @staticmethod
+    def create_global(fields):
+        subpage = Subpage()
+        subpage.forbid_fields(fields, ['id', 'contest', 'is_public'])
+        subpage.update_fields(fields, ['name', 'content', 'is_announcement', 'is_everywhere', 'order'])
+        subpage.save()
+        return subpage
+
+    @ExportMethod(DjangoStruct('Subpage'), [DjangoStruct('Subpage')], PCArgField('fields', 'contest', 'MANAGE'), [CannotSetField])
+    @staticmethod
+    def create_for_contest(fields):
+        subpage = Subpage()
+        subpage.forbid_fields(fields, ['id', 'is_everywhere'])
+        subpage.update_fields(fields, ['name', 'contest', 'content', 'is_announcement', 'is_public', 'order'])
+        subpage.save()
+        return subpage
+
+    @ExportMethod(DjangoStruct('Subpage'), [DjangoId('Subpage'), DjangoStruct('Subpage')], PCArg('self', 'MANAGE'), [CannotSetField])
+    def modify(self, fields):
+        if self.contest is None:
+            self.forbid_fields(fields, ['id', 'contest', 'is_public'])
+            self.update_fields(fields, ['name', 'content', 'is_announcement', 'is_everywhere', 'order'])
+        else:
+            self.forbid_fields(fields, ['id', 'contest', 'is_everywhere'])
+            self.update_fields(fields, ['name', 'content', 'is_announcement', 'is_public', 'order'])
+        self.save()
+        return self
+
+    #@ExportMethod(NoneType, [DjangoId('Subpage')], PCArg('self', 'MANAGE'), [CannotDeleteObject])
+    def delete(self):
+        logging.error('subpage deleted') #TODO: Waiting for non-cascading deletes in django
+        self.privileges.all().delete()
+        try:
+            super(Subpage, self).delete()
+        except DatabaseError:
+            raise CannotDeleteObject()
+
+    @ExportMethod(DjangoStructList('Subpage'), [bool], PCPermit())
+    @staticmethod
+    def get_global(announcements):
+        return Subpage.objects.filter(is_announcement=announcements, contest=None)
+
+    @ExportMethod(DjangoStructList('Subpage'), [DjangoId('Contest'), bool], PCPermit())
+    @staticmethod
+    def get_for_contest(contest, announcements):
+        return Subpage.objects.filter(is_announcement=announcements, contest=contest) | Subpage.objects.filter(is_announcement=announcements, contest=None, is_everywhere=True)
+
+class SubpageEvents(Events):
+    model = Subpage
+    on_insert = on_update = []
+    on_delete = []
