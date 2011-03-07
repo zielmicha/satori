13f1b9d2ce0fae853316460500d2f1a6ae0ed852
bartosz m walczak <bartosz.m.walczak@gmail.com>
2011-03-07 13:57:11 +0100

    refactoring: judge

---
+++ satori.javatool/src/satori/test/STestSnap.java
@@ -0,0 +1,93 @@
+package satori.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import satori.common.SAssert;
+import satori.common.SException;
+import satori.common.SListener1;
+import satori.common.SReference;
+import satori.common.SView;
+import satori.metadata.SInputMetadata;
+import satori.metadata.SJudge;
+import satori.thrift.STestData;
+
+public class STestSnap implements STestReader {
+	private long id;
+	private long problem_id;
+	private String name;
+	private SJudge judge;
+	private Map<SInputMetadata, Object> input;
+	private boolean complete;
+	
+	private final List<SView> views = new ArrayList<SView>();
+	private final List<SReference> refs = new ArrayList<SReference>();
+	private final List<SListener1<STestSnap>> deleted_listeners = new ArrayList<SListener1<STestSnap>>();
+	
+	@Override public boolean hasId() { return true; }
+	@Override public long getId() { return id; }
+	@Override public long getProblemId() { return problem_id; }
+	@Override public String getName() { return name; }
+	@Override public SJudge getJudge() { return judge; }
+	@Override public Map<SInputMetadata, Object> getInput() { return Collections.unmodifiableMap(input); }
+	
+	public boolean isComplete() { return complete; }
+	
+	private STestSnap() {}
+	
+	public static STestSnap create(STestReader source) {
+		STestSnap self = new STestSnap();
+		self.id = source.getId();
+		self.problem_id = source.getProblemId();
+		self.name = source.getName();
+		self.judge = source.getJudge();
+		self.input = source.getInput();
+		self.complete = true;
+		return self;
+	}
+	public static STestSnap createBasic(STestBasicReader source) {
+		STestSnap self = new STestSnap();
+		self.id = source.getId();
+		self.problem_id = source.getProblemId();
+		self.name = source.getName();
+		self.complete = false;
+		return self;
+	}
+	
+	public void set(STestReader source) {
+		SAssert.assertEquals(source.getId(), getId(), "Test ids don't match");
+		SAssert.assertEquals(source.getProblemId(), getProblemId(), "Problem ids don't match");
+		name = source.getName();
+		judge = source.getJudge();
+		input = source.getInput();
+		complete = true;
+		notifyModified();
+	}
+	public void setBasic(STestBasicReader source) {
+		SAssert.assertEquals(source.getId(), getId(), "Test ids don't match");
+		SAssert.assertEquals(source.getProblemId(), getProblemId(), "Problem ids don't match");
+		name = source.getName();
+		notifyModified();
+	}
+	public void reload() throws SException { set(STestData.load(id)); }
+	
+	private void notifyModified() {
+		for (SView view : views) view.update();
+		for (SReference ref : refs) ref.notifyModified();
+	}
+	public void notifyDeleted() {
+		for (SListener1<STestSnap> listener : deleted_listeners) listener.call(this);
+		for (SReference ref : refs) ref.notifyDeleted();
+	}
+	
+	public void addView(SView view) { views.add(view); }
+	public void removeView(SView view) { views.remove(view); }
+	
+	public void addDeletedListener(SListener1<STestSnap> listener) { deleted_listeners.add(listener); }
+	public void removeDeletedListener(SListener1<STestSnap> listener) { deleted_listeners.remove(listener); }
+	
+	public void addReference(SReference ref) { refs.add(ref); }
+	public void removeReference(SReference ref) { refs.remove(ref); }
+}
---
+++ satori.javatool/src/satori/thrift/SAttributeData.java
@@ -0,0 +1,92 @@
+package satori.thrift;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import satori.blob.SBlob;
+import satori.common.SException;
+import satori.metadata.SMetadata;
+import satori.session.SSession;
+import satori.thrift.gen.AnonymousAttribute;
+import satori.thrift.gen.Blob;
+import satori.type.STypeException;
+
+class SAttributeData {
+	static Map<String, AnonymousAttribute> convertAttrMap(Map<String, Object> attrs) throws SException {
+		Map<String, AnonymousAttribute> map = new HashMap<String, AnonymousAttribute>();
+		for (Map.Entry<String, Object> entry : attrs.entrySet()) {
+			if (entry.getValue() instanceof String) {
+				String str = (String)entry.getValue();
+				AnonymousAttribute dst = new AnonymousAttribute();
+				dst.setIs_blob(false);
+				dst.setValue(str);
+				map.put(entry.getKey(), dst);
+			}
+			if (entry.getValue() instanceof SBlob) {
+				SBlob blob = (SBlob)entry.getValue();
+				AnonymousAttribute dst = new AnonymousAttribute();
+				dst.setIs_blob(true);
+				dst.setFilename(blob.getName());
+				dst.setValue(blob.getHash());
+				map.put(entry.getKey(), dst);
+			}
+		}
+		return map;
+	}
+	static Map<String, SBlob> getBlobAttrMap(Map<String, AnonymousAttribute> attrs) {
+		Map<String, SBlob> result = new HashMap<String, SBlob>();
+		for (Map.Entry<String, AnonymousAttribute> entry : attrs.entrySet()) {
+			AnonymousAttribute attr = entry.getValue();
+			if (!attr.isIs_blob()) continue; //TODO: do something better
+			result.put(entry.getKey(), SBlob.createRemote(attr.getFilename(), attr.getValue()));
+		}
+		return result;
+	}
+	
+	private static class ExistsCommand implements SThriftCommand {
+		private final String hash;
+		private boolean result;
+		public ExistsCommand(String hash) { this.hash = hash; }
+		public boolean getResult() { return result; }
+		@Override public void call() throws Exception {
+			Blob.Iface iface = new Blob.Client(SThriftClient.getProtocol());
+			result = iface.Blob_exists(SSession.getToken(), hash);
+		}
+	}
+	private static boolean checkBlobExists(SBlob blob) throws SException {
+		ExistsCommand command = new ExistsCommand(blob.getHash());
+		SThriftClient.call(command);
+		return command.getResult();
+	}
+	
+	static Map<String, Object> createRawAttrMap(Map<? extends SMetadata, Object> attrs) throws SException {
+		Map<String, Object> result = new HashMap<String, Object>();
+		for (Map.Entry<? extends SMetadata, Object> entry : attrs.entrySet()) {
+			String key = entry.getKey().getName();
+			Object value = entry.getKey().getType().getRaw(entry.getValue());
+			result.put(key, value);
+		}
+		return result;
+	}
+	static <T extends SMetadata> Map<T, Object> createFormattedAttrMap(List<T> meta_list, Map<String, AnonymousAttribute> attrs) throws SException {
+		Map<T, Object> result = new HashMap<T, Object>();
+		for (T meta : meta_list) {
+			AnonymousAttribute attr = attrs.get(meta.getName());
+			if (attr == null) continue;
+			Object value = attr.isIs_blob() ? SBlob.createRemote(attr.getFilename(), attr.getValue()) : attr.getValue();
+            try { value = meta.getType().getFormatted(value); }
+            catch(STypeException ex) { continue; }
+			if (value != null) result.put(meta, value);
+		}
+		return result;
+	}
+	
+	static void createBlobs(Map<String, Object> attrs) throws SException {
+		for (Object value : attrs.values()) {
+			if (!(value instanceof SBlob)) continue;
+			SBlob blob = (SBlob)value;
+			if (!checkBlobExists(blob)) blob.saveRemote();
+		}
+	}
+}
