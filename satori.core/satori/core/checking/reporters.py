49ff56aa3dd4822ad360bfa562bfd30d7858f3f9
duraj <duraj@tcs.uj.edu.pl>
2011-03-11 12:48:12 +0100

    Added global problemsetters, user activation and status bars.

---
+++ satori.core/satori/core/checking/reporters.py
@@ -0,0 +1,166 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+import logging
+from blist import sortedlist
+from satori.core.checking.utils import RestTable
+
+class ReporterBase(object):
+    def __init__(self, test_suite_result):
+        super(ReporterBase, self).__init__()
+        self.test_suite_result = test_suite_result
+
+    def init(self):
+        pass
+
+    def accumulate(self, test_result):
+        pass
+
+    def status(self):
+        return True
+
+    def deinit(self):
+        pass
+
+class AssignmentReporter(ReporterBase):
+    def __init__(self, test_suite_result):
+        super(AssignmentReporter, self).__init__(test_suite_result)
+
+    def init(self):
+        self.test_suite_result.oa_set_str('status', 'ACC')
+        self.test_suite_result.status = 'ACC'
+        self.test_suite_result.report = ''
+        self.test_suite_result.save()
+
+    def accumulate(self, test_result):
+        pass
+        
+    def status(self):
+        return True
+
+    def deinit(self):
+        status = 'ACC'
+        report = ''
+        oa_map = self.test_suite_result.submit.overrides_get_map()
+        ostatus = oa_map.get('status', None)
+        if ostatus is not None and not ostatus.is_blob:
+            status = ostatus.value
+        oreport = oa_map.get('report', None)
+        if oreport is not None and not oreport.is_blob:
+            report = oreport.value
+        self.test_suite_result.status = status
+        self.test_suite_result.report = report
+        self.test_suite_result.save()
+        self.test_suite_result.oa_set_map(oa_map)
+
+class StatusReporter(ReporterBase):
+    def __init__(self, test_suite_result):
+        super(StatusReporter, self).__init__(test_suite_result)
+
+    def init(self):
+        self._status = 'OK'
+        self.test_suite_result.oa_set_str('status', 'QUE')
+        self.test_suite_result.status = 'QUE'
+        self.test_suite_result.report = ''
+        self.test_suite_result.save()
+
+    def accumulate(self, test_result):
+        status = test_result.oa_get_str('status')
+        logging.debug('Status Reporter %s: %s += %s', self.test_suite_result.id, self._status, status)
+        if status is None:
+            status = 'INT'
+        if self._status == 'OK' and status != 'OK':
+            self._status = status
+
+    def status(self):
+        return self._status == 'OK'
+
+    def deinit(self):
+        logging.debug('Status Reporter %s: %s', self.test_suite_result.id, self._status)
+        status = self._status
+        os = self.test_suite_result.submit.overrides_get('status')
+        if os is not None and not os.is_blob:
+            status = os.value
+        self.test_suite_result.oa_set_str('status', status)
+        self.test_suite_result.status = status
+        self.test_suite_result.report = 'Finished checking: {0}'.format(self._status)
+        self.test_suite_result.save()
+
+class MultipleStatusReporter(ReporterBase):
+    def __init__(self, test_suite_result):
+        super(MultipleStatusReporter, self).__init__(test_suite_result)
+
+    def init(self):
+        self._statuses = {}
+        self._status = 'OK'
+        self.test_suite_result.oa_set_str('status', 'QUE')
+        self.test_suite_result.status = 'QUE'
+        self.test_suite_result.report = ''
+        self.test_suite_result.save()
+
+    def accumulate(self, test_result):
+        test = test_result.test
+        code = TestMapping.objects().filter(test=test, suite= self.test_suite_result.suite)[0].order
+        status = test_result.oa_get_str('status')
+        self._statuses[code] = status
+        logging.debug('Status Reporter %s: %s += %s', self.test_suite_result.id, self._status, status)
+        if status is None:
+            status = 'INT'
+        if self._status == 'OK' and status != 'OK':
+            self._status = status
+
+    def status(self):
+        return True
+
+    def deinit(self):
+        logging.debug('Status Reporter %s: %s', self.test_suite_result.id, self._status)
+        status = self._status
+        os = self.test_suite_result.submit.overrides_get('status')
+        if os is not None and not os.is_blob:
+            status = os.value
+        self.test_suite_result.oa_set_str('status', status)
+        self.test_suite_result.status = status
+        report = u'Finished checking: {0}'.format(self._status)
+        report += ' (' + u', '.join([unicode(code) + ' ' + unicode(status) for (code, status) in self._statuses.objects().sorted()]) + ')'
+        self.test_suite_result.report = report
+        self.test_suite_result.save()
+
+class PointsReporter(ReporterBase):
+    def __init__(self, test_suite_result):
+        super(PointsReporter, self).__init__(test_suite_result)
+
+    def init(self):
+        self.checked = 0
+        self.passed = 0
+        self._status = ''
+        self.reportlines = sortedlist(key=lambda row: row[0])
+        self.test_suite_result.status = 'QUE'
+        self.test_suite_result.report = ''
+        self.test_suite_result.save()
+
+    def accumulate(self, test_result):
+        result = test_result.oa_get_str('status')
+        self.checked = self.checked+1
+        if result is None:
+            result = 'INT'
+        if result == 'OK':
+            self.passed = self.passed+1
+        self._status = str(self.passed) + ' / '+ str(self.checked)
+        
+        self.reportlines.add([test_result.test.name,result])
+        self.test_suite_result.report = ' '.join(['['+str(r[0])+':'+str(r[1])+']' for r in self.reportlines])
+        self.test_suite_result.save()
+        
+    def status(self):
+        return True
+
+    def deinit(self):
+        self.test_suite_result.status = self._status
+        self.test_suite_result.oa_set_str('status', self._status)
+        self.test_suite_result.oa_set_str('checked', self.checked)
+        self.test_suite_result.oa_set_str('passed', self.passed)
+        self.test_suite_result.save()
+
+
+reporters = {}
+for item in globals().values():
+    if isinstance(item, type) and issubclass(item, ReporterBase) and (item != ReporterBase):
+        reporters[item.__name__] = item
---
+++ satori.core/satori/core/entities/Submit.py
@@ -0,0 +1,120 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+
+from django.db import models
+
+from satori.core.dbev   import Events
+from satori.core.models import Entity, Contest
+
+@ExportModel
+class Submit(Entity):
+    """Model. Single problem solution (within or outside of a Contest).
+    """
+    parent_entity = models.OneToOneField(Entity, parent_link=True, related_name='cast_submit')
+
+    contestant    = models.ForeignKey('Contestant', related_name='submits')
+    problem       = models.ForeignKey('ProblemMapping', related_name='submits')
+    time          = models.DateTimeField(auto_now_add=True)
+
+    data          = AttributeGroupField(PCArg('self', 'VIEW'), PCDeny(), '')
+    overrides     = AttributeGroupField(PCArg('self', 'MANAGE'), PCDeny(), '')
+
+    class ExportMeta(object):
+        fields = [('contestant', 'VIEW'), ('problem', 'VIEW'), ('time', 'VIEW')]
+
+    @classmethod
+    def inherit_rights(cls):
+        inherits = super(Submit, cls).inherit_rights()
+        cls._inherit_add(inherits, 'VIEW', 'id', 'OBSERVE')
+        cls._inherit_add(inherits, 'OBSERVE', 'contestant', 'OBSERVE')
+        return inherits
+
+    def save(self, *args, **kwargs):
+        self.fixup_overrides()
+        self.fixup_data()
+        super(Submit, self).save(*args, **kwargs)
+
+    @ExportMethod(DjangoStruct('Submit'), [DjangoStruct('Submit'), Binary, unicode], PCArgField('fields', 'problem', 'SUBMIT'), [CannotSetField])
+    @staticmethod
+    def create(fields, content, filename):
+        submit = Submit()
+        submit.contestant = fields.problem.contest.find_contestant(token_container.token.role)
+        submit.forbid_fields(fields, ['id', 'contestant', 'time'])
+        submit.update_fields(fields, ['problem'])
+        submit.save()
+        Privilege.grant(submit.contestant, submit, 'VIEW')
+        blob = submit.data_set_blob('content', filename=filename)
+        blob.write(content)
+        blob.close()
+        RawEvent().send(Event(type='checking_new_submit', id=submit.id))
+        return submit
+
+    @ExportMethod(DjangoStruct('Submit'), [DjangoStruct('Submit'), unicode, unicode, TypedMap(DjangoId('Test'), TypedMap(unicode, AnonymousAttribute))], PCGlobal('ADMIN'), [CannotSetField])
+    @staticmethod
+    def inject(fields, content, filename, test_results):
+        submit = Submit()
+        submit.forbid_fields(fields, ['id'])
+        submit.update_fields(fields, ['contestant', 'time', 'problem'])
+        submit.save()
+        submit.update_fields(fields, ['time'])
+        submit.save()
+        Privilege.grant(submit.contestant, submit, 'VIEW')
+        blob = submit.data_set_blob('content', filename=filename)
+        blob.write(content)
+        blob.close()
+        for test, result in test_results.iteritems():
+            tr = TestResult(submit=submit, test=test, tester=token_container.token.role, pending=False)
+            tr.save()
+            tr.oa_set_map(result)
+        RawEvent().send(Event(type='checking_new_submit', id=submit.id))
+        return submit
+
+    @ExportMethod(NoneType, [DjangoId('Submit'), TypedMap(unicode, AnonymousAttribute)], PCAnd(PCArg('self', 'MANAGE'), PCEachValue('overrides', PCRawBlob('item'))))
+    def override(self, overrides):
+        self.overrides_set_map(overrides)
+        self.rejudge_test_suite_results()
+
+    @ExportMethod(DjangoStructList('TestResult'), [DjangoId('Submit'), DjangoId('TestSuite')], PCArg('self', 'VIEW'))
+    def get_test_suite_results(self, test_suite=None):
+        if test_suite is None:
+            test_suite = self.problem.default_test_suite
+        return TestResult.objects.filter(submit=self, test__testsuite=test_suite)
+            
+    @ExportMethod(DjangoStruct('TestSuiteResult'), [DjangoId('Submit'), DjangoId('TestSuite')], PCArg('self', 'VIEW'))
+    def get_test_suite_result(self, test_suite=None):
+        if test_suite is None:
+            test_suite = self.problem.default_test_suite
+        try:
+            return TestSuiteResult.objects.get(submit=self, test_suite=test_suite)
+        except TestSuiteResult.DoesNotExist:
+            return None
+
+    @ExportMethod(unicode, [DjangoId('Submit'), DjangoId('TestSuite')], PCArg('self', 'VIEW'))
+    def get_test_suite_status(self, test_suite=None):
+        test_suite_result = self.get_test_suite_result(test_suite)
+        if test_suite_result is None:
+            return None
+        return test_suite_result.status
+    
+    @ExportMethod(unicode, [DjangoId('Submit'), DjangoId('TestSuite')], PCArg('self', 'VIEW'))
+    def get_test_suite_report(self, test_suite=None):
+        test_suite_result = self.get_test_suite_result(test_suite)
+        if test_suite_result is None:
+            return None
+        return test_suite_result.report
+
+    @ExportMethod(ResultToRender, [DjangoId('Submit')], PCArg('self', 'OBSERVE'))
+    def get_result(self):
+        return self.problem.contest.submit_to_result_to_render(self)
+    
+    @ExportMethod(NoneType, [DjangoId('Submit')], PCArg('self', 'MANAGE'))
+    def rejudge_test_results(self):
+        RawEvent().send(Event(type='checking_rejudge_submit_test_results', id=self.id))
+
+    @ExportMethod(NoneType, [DjangoId('Submit')], PCArg('self', 'MANAGE'))
+    def rejudge_test_suite_results(self):
+        RawEvent().send(Event(type='checking_rejudge_submit_test_suite_results', id=self.id))
+
+class SubmitEvents(Events):
+    model = Submit
+    on_insert = on_update = ['owner', 'problem']
+    on_delete = []
