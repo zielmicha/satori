a6bee1f8063aa0b12ba0ba66b80f62d4ad8f1214
marek j wrobel <marek.j.wrobel@gmail.com>
2011-03-11 13:23:05 +0100

    Fixed ranking creation in athina import.

---
+++ satori.web/satori/web/views/contest/news.py
@@ -0,0 +1,56 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+from satori.client.common import want_import
+want_import(globals(), '*')
+from satori.web.utils.decorators import contest_view
+from satori.web.utils.shortcuts import fill_image_links
+from django import forms
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
+
+
+class ContestNewsEditForm(forms.Form):
+    name = forms.CharField(label="Message title")
+    content = forms.CharField(required=False,widget=forms.Textarea, label="Content")
+    is_sticky = forms.BooleanField(label="Always at the top", required=False)
+    is_public = forms.BooleanField(label="Show to all visitors", required=False)
+
+@contest_view
+def view(request, page_info):
+    messages = Web.get_subpage_list_for_contest(page_info.contest,True)
+    for m in messages:
+        m.html = fill_image_links(m.html, 'Subpage', m.subpage.id, 'content_files')
+    messages.sort(key=lambda m : [m.subpage.is_sticky, m.subpage.date_created], reverse=True)
+    return render_to_response('news.html',{'page_info' : page_info, 'messages' : messages })
+
+@contest_view
+def add(request, page_info):
+    if request.method=="POST":
+        form = ContestNewsEditForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Subpage.create_for_contest(SubpageStruct(is_announcement=True,contest=page_info.contest,name=data["name"],content=data["content"],is_sticky=data["is_sticky"]))
+            return HttpResponseRedirect(reverse('contest_news',args=[page_info.contest.id]))
+    else:
+        form = ContestNewsEditForm()
+    return render_to_response('news_create.html',{'page_info' : page_info, 'form' : form})
+
+@contest_view
+def edit(request, page_info,id):
+    message = Subpage.filter(SubpageStruct(id=int(id)))[0]
+    if request.method=="POST":
+        form = ContestNewsEditForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            message.modify(SubpageStruct(name=data["name"],content=data["content"],is_public=data["is_public"],is_sticky=data["is_sticky"]))
+            return HttpResponseRedirect(reverse('contest_news',args=[page_info.contest.id]))
+    else:
+        form = ContestNewsEditForm(initial={'name' : message.name, 'content' : message.content, 'is_public' : message.is_public, 'is_sticky' : message.is_sticky})
+    return render_to_response('news_edit.html',{'page_info' : page_info, 'form' : form, 'message' : message})
+
+@contest_view
+def delete(request, page_info,id):
+    messages = []
+    for message in Subpage.get_for_contest(page_info.contest,True):
+        messages.append([message,text2html(message.content)])
+    return render_to_response('news.html',{'page_info' : page_info, 'messages' : messages })
---
+++ satori.web/satori/web/views/login.py
@@ -0,0 +1,30 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+from satori.client.common import want_import
+want_import(globals(), '*')
+from satori.web.utils.decorators import general_view
+from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django import forms
+
+class LoginForm(forms.Form):
+    login = forms.CharField(label="Login:")
+    password = forms.CharField(widget=forms.PasswordInput, label="Password:")
+
+@general_view
+def view(request, page_info):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            login = form.cleaned_data['login']
+            password = form.cleaned_data['password']
+            try:
+                token = User.authenticate(login=login,password=password)
+                token_container.set_token(token)
+                return HttpResponseRedirect(reverse('news'))
+            except:
+                form.errors['__all__']= 'Login failed!'
+                return render_to_response('login.html', {'page_info' : page_info, 'form' : form, 'failed' : True })
+    else:
+        form = LoginForm()
+    return render_to_response('login.html', {'page_info' : page_info, 'form' : form })
