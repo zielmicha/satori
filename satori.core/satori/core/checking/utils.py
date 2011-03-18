d8b2f1a01df5f9007a6ed587915cbc49b9d77cd8
gzegzol <gzegzol@gmail.com>
2011-03-18 16:31:49 +0100

    marks improved.

---
+++ satori.core/satori/core/checking/utils.py
@@ -0,0 +1,63 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+
+class RestTable(object):
+    def __init__(self, *cols):
+        super(RestTable, self).__init__()
+        self.col_width = [col[0] for col in cols]
+        self.col_name = [col[1] for col in cols]
+        self.row_separator = '+' + '+'.join(['-' * width for width in self.col_width]) + '+\n'
+        self.header_separator = '+' + '+'.join(['=' * width for width in self.col_width]) + '+\n'
+        self.header_row = self.generate_row(*self.col_name)
+
+    def escape(self, item):
+        ret = []
+        for c in unicode(item):
+            if c.isalnum() or c.isspace():
+                ret.append(c)
+            else:
+                ret.append(u'\\')
+                ret.append(c)
+        return u''.join(ret)
+
+    def generate_row(self, *items):
+        if len(items) != len(self.col_width):
+            raise RuntimeError('Item count not equal to column count.')
+
+        max_count = 0
+        row_items = []
+
+        for i in range(len(items)):
+            item = unicode(items[i])
+
+            width = self.col_width[i]
+            row_item = []
+            first = 0
+            while first < len(item):
+                pos = item.rfind(' ', first, first + width)
+                if first + width >= len(item):
+                    item_elem = item[first:]
+                    first = len(item)
+                elif pos == -1:
+                    if item[first + width - 1] == u'\\':
+                        item_elem = item[first : first+width-1]
+                        first += width-1
+                    else:
+                        item_elem = item[first : first+width]
+                        first += width
+                else:
+                    item_elem = item[first : pos]
+                    first = pos + 1
+                item_elem = '|' + item_elem + ' ' * (width - len(item_elem))
+                row_item.append(item_elem)
+            row_items.append(row_item)
+            if max_count < len(row_item):
+                max_count = len(row_item)
+
+        for i in range(len(items)):
+            if len(row_items[i]) < max_count:
+                filling = '|' + ' ' * self.col_width[i]
+                while len(row_items[i]) < max_count:
+                    row_items[i].append(filling)
+
+        return ''.join([''.join([row_items[j][i] for j in range(len(items))]) + '|\n' for i in range(max_count)])
+
---
+++ satori.javatool/src/satori/thrift/SAttributeData.java
@@ -0,0 +1,108 @@
+package satori.thrift;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import satori.blob.SBlob;
+import satori.common.SAssertException;
+import satori.common.SException;
+import satori.metadata.SMetadata;
+import satori.session.SSession;
+import satori.thrift.gen.AnonymousAttribute;
+import satori.thrift.gen.Blob;
+
+class SAttributeData {
+	static AnonymousAttribute createAnonymousAttribute(Object value) {
+		if (value instanceof String) {
+			AnonymousAttribute result = new AnonymousAttribute();
+			result.setIs_blob(false);
+			result.setValue((String)value);
+			return result;
+		} else if (value instanceof SBlob) {
+			AnonymousAttribute result = new AnonymousAttribute();
+			result.setIs_blob(true);
+			result.setFilename(((SBlob)value).getName());
+			result.setValue(((SBlob)value).getHash());
+			return result;
+		} else throw new SAssertException("Invalid attribute class");
+	}
+	static Object convertAnonymousAttribute(AnonymousAttribute attr) {
+		return attr.isIs_blob() ? SBlob.createRemote(attr.getFilename(), attr.getValue()) : attr.getValue();
+	}
+	
+	static Map<String, AnonymousAttribute> convertAttrMap(Map<String, Object> attrs) throws SException {
+		Map<String, AnonymousAttribute> map = new HashMap<String, AnonymousAttribute>();
+		for (Map.Entry<String, Object> entry : attrs.entrySet()) {
+			if (entry.getValue() instanceof String) {
+				String str = (String)entry.getValue();
+				AnonymousAttribute dst = new AnonymousAttribute();
+				dst.setIs_blob(false);
+				dst.setValue(str);
+				map.put(entry.getKey(), dst);
+			}
+			if (entry.getValue() instanceof SBlob) {
+				SBlob blob = (SBlob)entry.getValue();
+				AnonymousAttribute dst = new AnonymousAttribute();
+				dst.setIs_blob(true);
+				dst.setFilename(blob.getName());
+				dst.setValue(blob.getHash());
+				map.put(entry.getKey(), dst);
+			}
+		}
+		return map;
+	}
+	static Map<String, SBlob> getBlobAttrMap(Map<String, AnonymousAttribute> attrs) {
+		Map<String, SBlob> result = new HashMap<String, SBlob>();
+		for (Map.Entry<String, AnonymousAttribute> entry : attrs.entrySet()) {
+			AnonymousAttribute attr = entry.getValue();
+			if (!attr.isIs_blob()) continue; //TODO: do something better
+			result.put(entry.getKey(), SBlob.createRemote(attr.getFilename(), attr.getValue()));
+		}
+		return result;
+	}
+	
+	private static class ExistsCommand implements SThriftCommand {
+		private final String hash;
+		private boolean result;
+		public ExistsCommand(String hash) { this.hash = hash; }
+		public boolean getResult() { return result; }
+		@Override public void call() throws Exception {
+			Blob.Iface iface = new Blob.Client(SThriftClient.getProtocol());
+			result = iface.Blob_exists(SSession.getToken(), hash);
+		}
+	}
+	private static boolean checkBlobExists(SBlob blob) throws SException {
+		ExistsCommand command = new ExistsCommand(blob.getHash());
+		SThriftClient.call(command);
+		return command.getResult();
+	}
+	
+	static Map<String, Object> createRemoteAttrMap(Map<? extends SMetadata, Object> attrs) throws SException {
+		Map<String, Object> result = new HashMap<String, Object>();
+		for (Map.Entry<? extends SMetadata, Object> entry : attrs.entrySet()) {
+			String key = entry.getKey().getName();
+			Object value = entry.getValue();
+			result.put(key, value);
+		}
+		return result;
+	}
+	static <T extends SMetadata> Map<T, Object> createLocalAttrMap(List<T> meta_list, Map<String, AnonymousAttribute> attrs) throws SException {
+		Map<T, Object> result = new HashMap<T, Object>();
+		for (T meta : meta_list) {
+			AnonymousAttribute attr = attrs.get(meta.getName());
+			if (attr == null) continue;
+			Object value = attr.isIs_blob() ? SBlob.createRemote(attr.getFilename(), attr.getValue()) : attr.getValue();
+			result.put(meta, value);
+		}
+		return result;
+	}
+	
+	static void createBlobs(Map<String, Object> attrs) throws SException {
+		for (Object value : attrs.values()) {
+			if (!(value instanceof SBlob)) continue;
+			SBlob blob = (SBlob)value;
+			if (!checkBlobExists(blob)) blob.saveRemote();
+		}
+	}
+}
