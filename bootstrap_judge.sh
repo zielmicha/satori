64c312237756d793be69b0cd62c5130ff02014c9
paladin <lech.duraj@gmail.com>
2011-05-13 15:18:52 +0200

    Added ranking parameters erasing.

---
+++ bootstrap_judge.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+#  bootstrap.sh -- build environment bootstrap
+#
+#  run this script _once_ after checking out the sources
+#  to prepare your work environment
+#
+
+cd "$(dirname "$(readlink -f "$0")")"
+aptitude -y install python-virtualenv python-dev libpq-dev libyaml-dev libcap-dev make
+unset PYTHONPATH
+virtualenv --no-site-packages .
+PVER=$(python --version 2>&1 |sed -e "s|^.*\\s\([0-9]\+\\.[0-9]\+\).*$|\1|")
+ln -s python "bin/python${PVER}"
+source bin/activate
+easy_install zc.buildout
+easy_install -U distribute
+mkdir -p src/python var/{buildout,cache}
+buildout -c buildout_judge.cfg
+(
+  cd satori.judge/runner
+  make runner
+  cp runner /usr/bin
+)
+cp satori.judge/satori/judge/default_judge.py /bin/judge
