5b73b8a6c35592130c2767e2dc248e5625a98e89
mammoth <mammoth@localhost>
2012-10-20 10:48:54 +0200

    repair ACM Board Ranking

---
+++ satori.ars/satori/ars/thrift/__init__.py
@@ -0,0 +1,10 @@
+# vim:ts=4:sts=4:sw=4:expandtab
+"""Provider for the thrift protocol.
+"""
+
+from reader import ThriftReader
+from writer import ThriftWriter
+from client import ThriftClient
+from client import ThriftHttpClient
+from server import ThriftServer
+
---
+++ satori.tools/satori/tools/submit/__init__.py
@@ -0,0 +1,53 @@
+import logging
+import os.path
+
+from satori.client.common import want_import
+from satori.tools import config, options, setup, auth_setup, catch_exceptions
+want_import(globals(), '*')
+
+@catch_exceptions
+def submit():
+    options.set_usage('Usage: %prog [options] problem_code submit_file')
+    options.add_argument('-C', '--contest', type=int, help='contest ID')
+
+    opts = setup(logging.CRITICAL)
+
+    contestId = None
+
+    if auth_setup.section:
+        if config.has_option(auth_setup.section, 'contest'):
+            contestId = config.getint(auth_setup.section, 'contest')
+
+    if opts.contest:
+    	contestId = options.contest
+
+    if not contestId:
+    	raise RuntimeError('The contest ID has not been specified')
+
+    if len(args) != 2:
+    	raise RuntimeError('Invalid number of positional parameters specified')
+
+    cc = Contest.filter(ContestStruct(id=contestId))
+    if not cc:
+    	raise RuntimeError('The specified contest is not found')
+
+    contest = cc[0]
+
+    pp = ProblemMapping.filter(ProblemMappingStruct(contest=contest, code=args[0]))
+    if not pp:
+    	raise RuntimeError('The specified problem is not found')
+
+    problem = pp[0]
+
+    f = open(args[1], 'r')
+    data = f.read()
+    f.close()
+
+    submit = Submit.create(SubmitStruct(problem=problem), data, os.path.basename(args[1]))
+
+    print 'Submit successfully created:'
+    print '  Contest:', contest.name
+    print '  Problem:', problem.code, '-', problem.title
+    print '  File:', args[1]
+    print '  Submit ID:', submit.id
+
